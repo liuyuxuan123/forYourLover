465269a77f7172f19e98c82bee4e75b2d57ed23f
刘宇轩 <liuyuxuan@promote.cache-dns.local>
2016-04-14 15:00:24 +0800

    Initial Commit

---
+++ forYourLover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  forYourLover
+//
+//  Created by 刘宇轩 on 16/4/14.
+//  Copyright © 2016年 刘宇轩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
